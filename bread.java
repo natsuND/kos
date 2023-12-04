8a1ea08389b6c0263a60cc0383e7badacd1f30be
User <gregoroik@gmail.com>
2023-12-04 12:03:07 +0200

    bread is good

--- bread.java
+++ bread.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class outputClass {
 	public static void main (String [] args) {
 		Scanner input = new Scanner (System.in);
+
+		//* arxizei o pinakas
 		
 		int[] pinakas = {1,25,9,8,33,7,8};
 		int [] theseis = new int[pinakas.length];
 		int searchvalue = 8 , theseiscount = 0;
 		
+		//* to mikos tou pinka
 		
 		for (int i=0; i<pinakas.length; i++) {
 			System.out.print(pinakas[i]+"");
